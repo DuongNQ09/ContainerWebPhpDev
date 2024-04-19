403d0356b5c2656b78886b7364bd50ca443bae59
DatDatVT <124223373+DatDatVT@users.noreply.github.com>
2024-04-19 14:31:01 +0700

    slide29.php

---
+++ slide29.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Advanced Web Development K15-N03</title>
+	</head>
+	<body>
+		<?php 	
+		sayHello('K15-N03 2024');
+		?>
+		<?php 
+        define("PI", 3.14);
+        $r = 10;
+        $s = PI*pow($r,2);
+        $p = 2*PI*$r;
+        ?>
+	</body>
+</html>
